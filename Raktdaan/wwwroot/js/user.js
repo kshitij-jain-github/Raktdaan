9d0baf8b451af3f58328eb712be005e0f85c2897
Kshitij Jain <jainkshitij1530@gmail.com>
2024-05-18 06:12:16 +0530

    ui changes

--- Raktdaan/wwwroot/js/user.js
+++ Raktdaan/wwwroot/js/user.js
@@ -1,39 +1,51 @@
 ﻿var dataTable;
-
 $(document).ready(function () {
     loadDataTable();
+
+    // Apply the search
+    $('#myTable thead .column-search').on('keyup change', function () {
+
+        var column = $(this).data('column'); // Get the column index
+        var value = $(this).val(); // Get the search value
+        dataTable.column(column).search(value).draw();
+     
+    });
+
+    // Prevent sorting when clicking on search inputs
+    $('#myTable thead .column-search').on('click', function (e) {
+        e.stopPropagation();
+
+     });
 });
- 
+
 function loadDataTable() {
-    dataTable = $('#myTable').DataTable(
-        {
+    dataTable = $('#myTable').DataTable({
         "ajax": {
             "url": "/Home/GetAll"
         },
         "columns": [
-            { "data": "full_Name"  },
+            { "data": "full_Name" },
             { "data": "blood_group" },
             { "data": "city" },
-            { "data": "state"},
+            { "data": "state" },
             { "data": "country" },
             {
                 "data": "id",
                 "render": function (data) {
                     return `
                         <div class="w-75 btn-group" role="group">
-                        <a href="/Product/Upsert?id=${data}"
-                        class="btn btn-danger btn-sm">  <i class="fa fa-pencil-square"></i> Get Contact</a>
-                     
-					</div>
-                        `
+                            <a href="/Product/Upsert?id=${data}" class="btn btn-danger btn-sm">
+                                <i class="fa fa-pencil-square"></i> Get Contact
+                            </a>
+                        </div>
+                    `;
                 },
-                
+ 
             }
-        ]
-    });
-}
 
+        ],
+     
 
-Object.assign(DataTable.defaults, {
-    paging: false,
-});
+        "paging": false, // Disable default paging
+     });
+} 
